b4e86852e169656f27bc097552b134d2c7e90220
Revalina Fidiya Anugrah <fanianugrah106@gmail.com>
2024-12-09 17:47:45 +0700

    create defender page

--- history.php
+++ history.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Splash Screen Example</title>
+    <title>History With Splash Screen</title>
     <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
     <style>
         /* Global Styles */
