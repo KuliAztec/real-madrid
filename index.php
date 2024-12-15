7f405840548e904752dcf90b6923ff96e92af114
KuliAztec <muhammad.setyoko@mhs.unsoed.ac.id>
2024-12-15 20:00:42 +0700

    Bug Fixing-nth

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header('Location: tampilan-depan.php');
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
